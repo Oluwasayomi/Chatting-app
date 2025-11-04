0cbf330cf36d7057f38986220c36d5a6a315bc69
Oluwasayomi <ayandearie@yahoo.com>
2025-11-04 23:07:35 +0000

    Check token for logged in
    Check token to confirm user logged in

--- api/index.js
+++ api/index.js
@@ -23,6 +23,19 @@ app.get('/test', (req,res) => {
     res.json('test ok');
 });
 
+app.get('/profile', (req,res) => {
+    const token = req.cookies?.token;
+    if (token) {
+        jwt.verify(token, jwtSecret, {}, (err, userData) =>{
+        if (err) throw err;
+        res.json(userData);
+    });
+
+    } else {
+        res.status(401).json('no token');
+    }
+});
+
 app.post('/register', async(req,res) => {
     const {username,password} = req.body;
     try {
