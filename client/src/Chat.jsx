6eea0295eb53d049338e4af19671299b00ef144f
Oluwasayomi <ayandearie@yahoo.com>
2025-11-11 22:24:15 +0000

    Colour change for chat page layout
    Colour change for chat page layout

--- client/src/Chat.jsx
+++ client/src/Chat.jsx
@@ -1,11 +1,11 @@
 export default function Chat() {
     return (
         <div className="flex h-screen">
-            <div className="bg-green-400 w-1/3">
+            <div className="bg-amber-400 w-1/3">
                 Contacts
             </div>
             
-            <div className="flex flex-col bg-amber-400 w-2/3 p-2">
+            <div className="flex flex-col bg-green-400 w-2/3 p-2">
                 <div className="flex-grow">
                     Messages with selected person
                 </div>
@@ -13,7 +13,7 @@ export default function Chat() {
                     <input type="text" 
                     placeholder="Type your message here" 
                     className="bg-white flex-grow border p-2"/>
-                    <button className="bg-green-400 p-2 text-white">
+                    <button className="bg-amber-500 p-2 text-white">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
                             <path strokeLinecap="round" strokeLinejoin="round" d="M6 12 3.269 3.125A59.769 59.769 0 0 1 21.485 12 59.768 59.768 0 0 1 3.27 20.875L5.999 12Zm0 0h7.5" />
                         </svg>
