98f391334edd877875c05142fd9a407b2adaa2f1
Oluwasayomi <ayandearie@yahoo.com>
2025-11-11 22:20:49 +0000

    Chat page layout setup
    Chat page layout setup

--- client/src/Chat.jsx
+++ client/src/Chat.jsx
@@ -1,5 +1,25 @@
 export default function Chat() {
     return (
-        <div>Chat here</div>
+        <div className="flex h-screen">
+            <div className="bg-green-400 w-1/3">
+                Contacts
+            </div>
+            
+            <div className="flex flex-col bg-amber-400 w-2/3 p-2">
+                <div className="flex-grow">
+                    Messages with selected person
+                </div>
+                <div className="flex gap-2">
+                    <input type="text" 
+                    placeholder="Type your message here" 
+                    className="bg-white flex-grow border p-2"/>
+                    <button className="bg-green-400 p-2 text-white">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M6 12 3.269 3.125A59.769 59.769 0 0 1 21.485 12 59.768 59.768 0 0 1 3.27 20.875L5.999 12Zm0 0h7.5" />
+                        </svg>
+                    </button>
+                </div>
+            </div>
+        </div>
     );
 };
