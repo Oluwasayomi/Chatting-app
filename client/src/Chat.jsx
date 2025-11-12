53bd8abe6c4f9145646aac9d1dbfdde1b1ec29ee
Oluwasayomi <ayandearie@yahoo.com>
2025-11-12 23:47:29 +0000

    Rounded buttons for send
    Rounded corner for buttons

--- client/src/Chat.jsx
+++ client/src/Chat.jsx
@@ -12,8 +12,8 @@ export default function Chat() {
                 <div className="flex gap-2">
                     <input type="text" 
                     placeholder="Type your message here" 
-                    className="bg-white flex-grow border p-2"/>
-                    <button className="bg-amber-500 p-2 text-white">
+                    className="bg-white flex-grow border rounded-sm p-2"/>
+                    <button className="bg-amber-500 p-2 text-white rounded-sm">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
                             <path strokeLinecap="round" strokeLinejoin="round" d="M6 12 3.269 3.125A59.769 59.769 0 0 1 21.485 12 59.768 59.768 0 0 1 3.27 20.875L5.999 12Zm0 0h7.5" />
                         </svg>
