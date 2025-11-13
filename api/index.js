fdf2dda9e89088386a1af626fcde5db10cb1b7fc
Oluwasayomi <ayandearie@yahoo.com>
2025-11-13 00:51:46 +0000

    Websocket connections
    Websocket connections

--- api/index.js
+++ api/index.js
@@ -97,7 +97,22 @@ app.post('/register', async(req,res) => {
 const server = app.listen(4000);
 
 const wss = new ws.WebSocketServer({server});
-wss.on('connection', (connection) => {
-    console.log('connected');
-    connection.send('hello');
+wss.on('connection', (connection, req) => {
+    const cookies = req.headers.cookie;
+    if (cookies) {
+        const tokenCookieString = cookies.split(';').find(str => str.startsWith('token='));
+        if (tokenCookieString) {
+            const token = tokenCookieString.split('=')[1];
+            if (token) {
+                jwt.verify(token, jwtSecret, {}, (err, userData) => {
+                    if (err) throw err;
+                    const {userId, username} = userData;
+                    connection.userId = userId;
+                    connection.username = username;
+                });
+            }
+        }
+    }
+
+    console.log([...wss.clients].map(c => c.username));
 });
