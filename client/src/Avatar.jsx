14499f1fc149e557a04a3857672728b191dc81e3
Oluwasayomi <ayandearie@yahoo.com>
2025-11-14 21:51:35 +0000

    User initial added to avatar
    User initial added to avatar

--- client/src/Avatar.jsx
+++ client/src/Avatar.jsx
@@ -1,5 +1,7 @@
-export default function Avatar({userId}) {
+export default function Avatar({userId, username}) {
     return (
-        <div className="w-8 h-8 bg-green-400 rounded-full"></div>
+        <div className="w-8 h-8 bg-green-400 rounded-full flex items-center">
+            <div className="text-center w-full">{username[0]} </div>
+        </div>
     );
 };
