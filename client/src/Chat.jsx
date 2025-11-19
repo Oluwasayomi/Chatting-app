601f9f9f8627391c9fa2bffc3575db22f6c29050
Oluwasayomi <ayandearie@yahoo.com>
2025-11-19 23:04:32 +0000

    Select user function w/colour on select
    Select user function w/colour on select

--- client/src/Chat.jsx
+++ client/src/Chat.jsx
@@ -4,6 +4,7 @@ import Avatar from "./Avatar";
 export default function Chat() {
     const [ws, setWs] = useState(null);
     const [onlinePeople, setOnlinePeople] = useState({});
+    const [selectedUserId, setSelectedUserId] = useState(null);
 
     useEffect(() => {
         const ws = new WebSocket('ws://localhost:4000');
@@ -37,7 +38,7 @@ export default function Chat() {
                 </div>
                 <h1 className="text-bold text-2xl border-b-5 border-green-400 pb-2">Contacts</h1>
                 {Object.keys(onlinePeople).map(userId => (
-                    <div key={userId} className="border-b border-green-200 py-2 flex items items-center gap-2 cursor-pointer">
+                    <div onClick={() => setSelectedUserId(userId)} key={userId} className={"border-b border-green-200 py-2 flex items items-center gap-2 cursor-pointer " +(userId === selectedUserId ? 'bg-green-400' : '')}>
                         <Avatar username={onlinePeople[userId]} userId={userId}/>
                         <span className="text-grey-800">{onlinePeople[userId]}</span>
                     </div>
