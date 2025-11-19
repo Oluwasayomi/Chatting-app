80949276c8b8d9d348cb9dc6f2332b232160db28
Oluwasayomi <ayandearie@yahoo.com>
2025-11-19 23:09:31 +0000

    Changed padding for contacts side
    Changed weird padding for contacts side

--- client/src/Chat.jsx
+++ client/src/Chat.jsx
@@ -29,16 +29,18 @@ export default function Chat() {
 
     return (
         <div className="flex h-screen">
-            <div className="bg-amber-400 w-1/3 pl-2 pt-4">
-                <div className="text-green-800 font-bold flex gap-2 mb-4">
+            <div className="bg-amber-400 w-1/3">
+                <div className="text-green-800 font-bold flex gap-2 p-4">
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6">
                     <path fillRule="evenodd" d="M12 2.25c-2.429 0-4.817.178-7.152.521C2.87 3.061 1.5 4.795 1.5 6.741v6.018c0 1.946 1.37 3.68 3.348 3.97.877.129 1.761.234 2.652.316V21a.75.75 0 0 0 1.28.53l4.184-4.183a.39.39 0 0 1 .266-.112c2.006-.05 3.982-.22 5.922-.506 1.978-.29 3.348-2.023 3.348-3.97V6.741c0-1.947-1.37-3.68-3.348-3.97A49.145 49.145 0 0 0 12 2.25ZM8.25 8.625a1.125 1.125 0 1 0 0 2.25 1.125 1.125 0 0 0 0-2.25Zm2.625 1.125a1.125 1.125 0 1 1 2.25 0 1.125 1.125 0 0 1-2.25 0Zm4.875-1.125a1.125 1.125 0 1 0 0 2.25 1.125 1.125 0 0 0 0-2.25Z" clipRule="evenodd" />
                     </svg>
                     ChatApp
                 </div>
-                <h1 className="text-bold text-2xl border-b-5 border-green-400 pb-2">Contacts</h1>
+                <h1 className="text-bold text-2xl border-b-5 border-green-400 pb-2 pl-4">Contacts</h1>
                 {Object.keys(onlinePeople).map(userId => (
-                    <div onClick={() => setSelectedUserId(userId)} key={userId} className={"border-b border-green-200 py-2 flex items items-center gap-2 cursor-pointer " +(userId === selectedUserId ? 'bg-green-400' : '')}>
+                    <div onClick={() => setSelectedUserId(userId)} 
+                        key={userId} 
+                        className={"border-b border-green-200 py-2 pl-4 flex items items-center gap-2 cursor-pointer " +(userId === selectedUserId ? 'bg-green-400' : '')}>
                         <Avatar username={onlinePeople[userId]} userId={userId}/>
                         <span className="text-grey-800">{onlinePeople[userId]}</span>
                     </div>
