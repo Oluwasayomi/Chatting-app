77a6ad90582c78a668bf7af6894c1d9cfd817ef2
Oluwasayomi <ayandearie@yahoo.com>
2025-12-01 21:39:26 +0000

    Send message function
    Send message function

--- client/src/Chat.jsx
+++ client/src/Chat.jsx
@@ -7,6 +7,7 @@ export default function Chat() {
     const [ws, setWs] = useState(null);
     const [onlinePeople, setOnlinePeople] = useState({});
     const [selectedUserId, setSelectedUserId] = useState(null);
+    const [newMessageText, setNewMessageText] = useState('');
     const {username, id} = useContext(UserContext);
 
     useEffect(() => {
@@ -29,6 +30,16 @@ export default function Chat() {
             showOnlinePeople(messageData.online);
         }
     };
+
+    function sendMessage(ev) {
+        ev.preventDefault();
+        ws.send(JSON.stringify({
+            message: {
+                recipient: selectedUserId,
+                text: newMessageText,
+            }
+        }));
+    }
 
     const onlinePeopleExclOurUser = {...onlinePeople};
     delete onlinePeopleExclOurUser[id];
@@ -61,16 +72,18 @@ export default function Chat() {
                         </div>
                     )}
                 </div>
-                <div className="flex gap-2">
+                <form className="flex gap-2" onSubmit={sendMessage}>
                     <input type="text" 
+                    value={newMessageText}
+                    onChange={ev => setNewMessageText(ev.target.value)}
                     placeholder="Type your message here" 
                     className="bg-white flex-grow border rounded-sm p-2"/>
-                    <button className="bg-amber-500 p-2 text-white rounded-sm">
+                    <button type="submit" className="bg-amber-500 p-2 text-white rounded-sm">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
                             <path strokeLinecap="round" strokeLinejoin="round" d="M6 12 3.269 3.125A59.769 59.769 0 0 1 21.485 12 59.768 59.768 0 0 1 3.27 20.875L5.999 12Zm0 0h7.5" />
                         </svg>
                     </button>
-                </div>
+                </form>
             </div>
         </div>
     );
