86527bbf4de073f4bf75c609f8df94a620a87c1d
Oluwasayomi <ayandearie@yahoo.com>
2025-12-22 06:02:55 +0000

    Store sender and recipient in handle message

--- client/src/Chat.jsx
+++ client/src/Chat.jsx
@@ -32,7 +32,7 @@ export default function Chat() {
         if ('online' in messageData) {
             showOnlinePeople(messageData.online);
         } else if ('text' in messageData) {
-            setMessages(prev => ([...prev, {isOur:false, text:messageData.text}]));
+            setMessages(prev => ([...prev, {...messageData}]));
         }
     };
 
@@ -43,7 +43,10 @@ export default function Chat() {
             text: newMessageText,
         }));
         setNewMessageText('');
-        setMessages(prev => ([...prev,{text: newMessageText, isOur: true}]));
+        setMessages(prev => ([...prev,{
+            text: newMessageText, 
+            isOur: true
+        }]));
     }
 
     const onlinePeopleExclOurUser = {...onlinePeople};
@@ -81,7 +84,9 @@ export default function Chat() {
                     {!!selectedUserId && (
                         <div>
                             {messagesWithoutDupes.map(message =>
-                                <div>{message.text}</div>
+                                <div>
+                                    {message.text}
+                                </div>
                             )}
                         </div>
                     )}
