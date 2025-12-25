dbb3a6b70af33089c9b2e6ae6325a6f971062536
Oluwasayomi <ayandearie@yahoo.com>
2025-12-25 03:14:51 +0000

    Fetch past messages (message history)
    Show message history with user

--- api/index.js
+++ api/index.js
@@ -57,9 +57,15 @@ app.get('/test', (req,res) => {
     res.json('test ok');
 });
 
-app.get('/messages/:userId', (req,res) => {
+app.get('/messages/:userId', async (req,res) => {
     const {userId} = req.params;
     const userData = getUserDataFromRequest(req);
+    const ourUserId = userData.userId;
+    const messages = await Message.find({
+        sender: {$in:[userId,ourUserId]},
+        recipient: {$in:[userId,ourUserId]},
+    }).sort({createdAt:-1});
+    res.json(messages);
 });
 
 app.get('/profile', (req,res) => {
