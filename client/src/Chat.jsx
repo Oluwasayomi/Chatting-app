4902e66ebeffce3fa60f4a892a209ff1f01002f7
Oluwasayomi <ayandearie@yahoo.com>
2025-12-27 03:57:57 +0000

    Added Timeout for reconnecting

--- client/src/Chat.jsx
+++ client/src/Chat.jsx
@@ -21,7 +21,12 @@ export default function Chat() {
         const ws = new WebSocket('ws://localhost:4000');
         setWs(ws);
         ws.addEventListener('message', handleMessage);
-        ws.addEventListener('close', () => connectToWS());
+        ws.addEventListener('close', () => {
+            setTimeout(() => {
+                console.log('Disconnected. Trying to reconnect.');
+                connectToWS();
+            }, 1000);
+        });
     }
 
     function showOnlinePeople(peopleArray) {
