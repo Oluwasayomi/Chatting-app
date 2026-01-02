0e7f8f7972f0947dded37251038d85b45d463ec8
Oluwasayomi <ayandearie@yahoo.com>
2026-01-02 22:22:44 +0000

    Logout button setup

--- client/src/Chat.jsx
+++ client/src/Chat.jsx
@@ -101,30 +101,36 @@ export default function Chat() {
 
     return (
         <div className="flex h-screen">
-            <div className="bg-amber-400 w-1/3">
-                <Logo />
-                <h1 className="text-bold text-2xl border-b-5 border-green-400 pb-2 pl-4">Contacts</h1>
-                {Object.keys(onlinePeopleExclOurUser).map(userId => (
-                    <Contact 
-                    key={userId}
-                    id= {userId}
-                    online={true}
-                    username={onlinePeopleExclOurUser[userId]}
-                    onClick={() => setSelectedUserId(userId)}
-                    selected={userId === selectedUserId}
-                    /> 
-                ))}
+            <div className="bg-amber-400 w-1/3 flex flex-col">
+                <div className="flex-grow">
+                    <Logo />
+                    <h1 className="text-bold text-2xl border-b-5 border-green-400 pb-2 pl-4">Contacts</h1>
+                    {Object.keys(onlinePeopleExclOurUser).map(userId => (
+                        <Contact 
+                        key={userId}
+                        id= {userId}
+                        online={true}
+                        username={onlinePeopleExclOurUser[userId]}
+                        onClick={() => setSelectedUserId(userId)}
+                        selected={userId === selectedUserId}
+                        /> 
+                    ))}
 
-                {Object.keys(offlinePeople).map(userId => (
-                    <Contact 
-                    key={userId}
-                    id= {userId}
-                    online={false}
-                    username={offlinePeople[userId].username}
-                    onClick={() => setSelectedUserId(userId)}
-                    selected={userId === selectedUserId}
-                    /> 
-                ))}
+                    {Object.keys(offlinePeople).map(userId => (
+                        <Contact 
+                        key={userId}
+                        id= {userId}
+                        online={false}
+                        username={offlinePeople[userId].username}
+                        onClick={() => setSelectedUserId(userId)}
+                        selected={userId === selectedUserId}
+                        /> 
+                    ))}
+                </div>
+
+                <div className="p-2 text-center">
+                    <button className="text-sm bg-green-300 py-1 px-2 text-gray-700 border rounded-sm">Logout</button>
+                </div>
             </div>
             
             <div className="flex flex-col bg-green-400 w-2/3 p-2">
