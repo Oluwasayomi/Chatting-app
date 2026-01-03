b5d79af7f6421aabf49bb306c57ef7c82dd2eb05
Oluwasayomi <ayandearie@yahoo.com>
2026-01-03 21:50:39 +0000

    Style user/logout
    Styles username and logout button section

--- client/src/Chat.jsx
+++ client/src/Chat.jsx
@@ -135,16 +135,16 @@ export default function Chat() {
                     ))}
                 </div>
 
-                <div className="p-2 text-center flex items-center">
-                    <span className="mr-2 text-sm text-gray-800 flex">
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6">
+                <div className="p-2 text-center flex items-center justify-center">
+                    <span className="mr-2 text-sm text-gray-800 flex items-center">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-4 h-4">
                             <path fillRule="evenodd" d="M7.5 6a4.5 4.5 0 1 1 9 0 4.5 4.5 0 0 1-9 0ZM3.751 20.105a8.25 8.25 0 0 1 16.498 0 .75.75 0 0 1-.437.695A18.683 18.683 0 0 1 12 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 0 1-.437-.695Z" clipRule="evenodd" />
                         </svg>
                         {username}
                     </span>
                     <button 
                     onClick={logout}
-                    className="text-sm bg-green-400 py-1 px-2 text-gray-700 border rounded-sm">
+                    className="text-sm bg-green-400 py-1 px-2 text-gray-700 border border-amber-500 rounded-sm">
                         Logout
                     </button>
                 </div>
